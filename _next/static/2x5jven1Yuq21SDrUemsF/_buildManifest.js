8b6680e58c0754597bcd18ab9d7cad6e8dfe61b3
John Marvin Pino <Marvin@asi.com>
2023-05-03 17:30:17 +0800

    init commit

---
+++ _next/static/2x5jven1Yuq21SDrUemsF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-6d1727bb89913d4c.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],"/about":["static/chunks/pages/about-45febdd3b6d0a2f2.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
