c4bdc27855df0b08c357b07535dc9fa551a2c8ef
cgoatni <mixtape2016@gmail.com>
2024-07-16 15:38:47 +0800

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ function App() {
 
   return (
     <>
-      <h1>Greetings From CGOATNI</h1>
-      <h2>Hello World!</h2>
+      {/* <h1>Greetings From CGOATNI</h1>
+      <h2>Hello World!</h2> */}
     </>
   )
 }
