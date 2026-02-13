ad3d5246306a8f58270a77ccdf541f0b411d0f4d
cgoatni <mixtape2016@gmail.com>
2026-02-14 05:21:35 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/portfolio/', // MUST match your GitHub repo name
+  base: '/portfolio/',
   plugins: [react()],
 });
